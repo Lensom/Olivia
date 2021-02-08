4f809f952026f9607f1c70347aef98c6d1c3eff9
Lensom <magurawork@gmail.com>
2021-02-08 22:38:30 +0300

    Add toggle functionality for menu

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './components/Menu';
+import cn from 'classnames';
 import styles from './Dashboard.module.scss';
 
 
 export const Dashboard: React.FC = () => {
-
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
   return (
     <div className={styles.dashboard}>
-      <div className="menu">
+      <div className={styles.icon} onClick={() => setToggleMenu(prev => !prev)} />
+      <div className={cn([styles.menu], {[styles.show]: toggleMenu})}>
         <Menu />
       </div>
     </div>
